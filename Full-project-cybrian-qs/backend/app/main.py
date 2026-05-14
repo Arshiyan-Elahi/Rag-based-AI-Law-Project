edff6104d9b25d9dc667668f0e2faf2caad9c527
Arshiyan-Elahi <arshiyanelahi84@gmail.com>
2026-05-14 12:45:52 +0500

    update docker deployment configuration

--- Full-project-cybrian-qs/backend/app/main.py
+++ Full-project-cybrian-qs/backend/app/main.py
@@ -186,7 +186,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173" , 
+    "http://65.21.244.158",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
